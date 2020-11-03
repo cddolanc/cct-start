e4aa65389342c1bb76825bebdf3d6911ced9b3bf
cddolanc <cddolanc@gmail.com>
2020-11-03 18:44:59 +0000

    start

---
+++ wk6.py
@@ -0,0 +1,10 @@
+# from math import sqrt as square_root # Brings the whole math module in
+
+# print(square_root(4)) # Prints the square root of 4, which is 2
+
+from core import calc as super_func, iterate
+
+print(super_func(10,15))
+
+
+iterate()
