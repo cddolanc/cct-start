ce9cfb362008ddd4782ebce38051d5477488675e
cddolanc <cddolanc@gmail.com>
2020-11-17 18:22:36 +0000

    update

---
+++ wk8/wk8.py
@@ -0,0 +1,43 @@
+# try:
+#     f = open('testfile','w')
+#     f.write('Test write this!')
+# except IOError:
+#     # This will only check for an IOError exception and then execute this print statement
+#     print("Error: Could not find file or read data")
+# else:
+#     print("Content written successfully")
+#     f.close()
+
+# try:
+#     f = open('testfile','r')
+#     f.write('Test write this')
+# except:
+#     # This will only check for an IOError exception and then execute this print statement
+#     print("Error: Could not find file or read data")
+# else:
+#     print("Content written successfully")
+#     f.close()
+
+# try:
+#     f = open("testfile", "r")
+#     f.write("Test write statement")
+#     f.close()
+# finally:
+#     print("Always execute finally code blocks")
+
+def askint():
+    while True:
+        try:
+            val = int(input("Please enter an integer: "))
+        except:
+            print("Looks like you did not enter an integer!")
+            continue
+        else:
+            print("Yep That's an integer!")
+            print(val)
+            break
+        finally:
+            print("Finally, I executed!")
+        print(val)
+
+askint()
