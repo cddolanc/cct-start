dac6aae5a43acd2fa1b88779fa4e5638c1b6ed86
cddolanc <cddolanc@gmail.com>
2020-11-27 16:04:19 +0000

    start

---
+++ wk3.py
@@ -0,0 +1,160 @@
+# sum_value = 5 + 8
+# print(sum_value) # Prints 8
+
+# sum_2 = 6 + sum_value 
+# print(sum_2) # Prints: 14 
+
+
+# my_list = [1,2,3,4] # Initialize my_list
+
+# my_list_2 = [5,6,7,8] # Initialize my_list_2
+
+# my_list = my_list + my_list_2 # Take the current value of my_list and add my_list_2 to it
+
+# print(my_list) # Prints: [1, 2, 3, 4, 5, 6, 7, 8]
+
+
+
+
+# name = "Hello " # Set name variable to an string 'hello '
+
+# name = name + "World!" # Take the current name value and add 'world' to it
+
+# print(name) # Prints: Hello World!
+
+
+
+# my_list = [] # Initialize an empty list
+
+# name = "John" # Initialize name variable to John
+
+# my_list = my_list + name # Adding the current my_list variable to the string name
+
+# print(my_list) # Prints: ['j','o','h','n']
+
+
+# print(5 - 3) # Prints: 2
+
+# value = 5 - 8
+# print(value) # Prints -3
+
+# value_2 = value - 6 
+# print(value_2) # Prints: -9
+
+
+# print(5 * 3) # Prints: 15
+
+# value = 5 * 8 
+# print(value) # Prints 40
+
+
+# value_2 = value * 6
+# print(value_2) # Prints: 120
+
+
+# my_list = [1,2,3,4] # Initialize my_list
+
+# my_list = my_list * 3 # Take my_list and multiply it by 3
+
+# print(my_list) # Prints: [1,2,3,4,1,2,3,4,1,2,3,4]
+
+
+
+
+# print(5 / 2) # Prints: 2.5
+
+
+# value = 5 / 3
+# print(value) # Prints 1.6666666666666667
+
+# value_2 = value / 2 
+# print(value_2) # Prints: 0.8333333333333334
+
+
+# print(5 // 3) # Prints: 1
+
+# value = 5 // 8
+# print(value) # Prints 0 (Remember it ALWAYS rounds down)
+
+
+# value_2 = value // 6 
+
+# print(value_2) # Prints: 0
+
+
+
+
+# variable_1 = 50 # Initialize the variable to 5
+
+# variable_1 = variable_1 + 5 # Take the current value of the variable and add 5
+
+# variable_1 = variable_1 - 5 # Take the current value of the variable and subtract 5
+
+# variable_1 = variable_1 / 2 # Take the current value of the variable and floating point divide by 2
+
+# variable_1 = variable_1 // 2 # Take the current value of the variable and integer divide by 2
+
+# print(variable_1)
+
+
+
+
+# variable_1 = 5 # Initialize the variable to 5
+# print(variable_1)
+# variable_1 += 5 # Take the current value of the variable and add 5
+# print(variable_1)
+# variable_1 -= 5 # Take the current value of the variable and subtract 5
+# print(variable_1)
+# variable_1 /= 2 # Take the current value of the variable and floating point divide by 2
+# print(variable_1)
+# variable_1 //= 2 # Take the current value of the variable and integer divide by 2
+# print(variable_1)
+
+
+#MODULUS
+
+# print(5 % 3) # Prints: 2
+
+# value = 10 % 5
+
+# print(value) # Prints 0 (Therefore 10 is evenly divisible by 5)
+
+
+# value_2 = value % 6 
+
+# print(value_2) # Prints: 0 (Because 0/anything is always 0)
+
+
+# print(5 > 3) # Prints: True; since 5 is greater than 3
+
+# result = 5 > 3 # You can store the result in a variable
+
+# print(result) # Prints: True
+
+# print(3 > 5) # Prints: False; since 3 is NOT greater than 5
+
+# print(5 > 5) # Prints: False; since 5 is NOT greater than 5 (they are equal)
+
+
+
+# print(5 >= 3) # Prints: True; since 5 is greater than 3
+
+# result = 5 >= 3 # You can store the result in a variable
+
+# print(result) # Prints: True
+
+# print(3 >= 5) # Prints: False; since 3 is NOT greater than 5
+
+# print(5 >= 5) # Prints: True; since 5 is equal to 5
+
+
+
+print(5 < 3) # Prints: False; since 5 is NOT less than 3
+
+result = 5 < 3 # You can store the result in a variable
+
+print(result) # Prints: False
+
+print(3 < 5) # Prints: True; since 3 is less than 5
+
+print(5 < 5) # Prints: False; since 5 is NOT less than 5 (they are equal)
