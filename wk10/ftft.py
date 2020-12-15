292cbf51359f1b6c378bd78c7fe853321cd4beda
cddolanc <cddolanc@gmail.com>
2020-12-15 12:36:12 +0000

    sd

--- wk10/ftft.py
+++ wk10/ftft.py
@@ -16,6 +16,32 @@ winner = None
 # Whos turn is it?
 current_player = 'X'
 
+# The game........................
+def play_game():
+
+    # Display initial board
+    display_board()
+
+
+    #While the game continues.....
+    while game_continues:
+        
+        # Handle the turn of the current player
+        handle_turn(current_player)
+        
+        # Check if the game is over
+        check_game_over()
+
+        # Flip to the other player
+        flip_player()
+
+    # The Game has ended
+    if winner == 'X' or winner == 'O':
+        print(winner + ' won.')
+    elif winner == None:
+        print('The Game Is A Tie.')
+
+
 def display_board():
     bright_yellow = "\033[0;93m"
     green = "\033[0;32m"
@@ -30,40 +56,6 @@ def display_board():
     print(green  + '     |     |')
     print(green  + '1: ' + bright_yellow +  board[1] + green  +' |2: '+ bright_yellow  + board[2] + green  +' |3: '+ bright_yellow + board[3])
     print(green  + '     |     |')
-
-
-
-
-# The game........................
-def play_game():
-
-    # Display initial board
-    display_board()
-
-
-    #While the game continues.....
-    while game_continues:
-        
-
-        # Handle the turn of the current player
-        handle_turn(current_player)
-        
-
-        # Check if the game is over
-        check_game_over()
-
-
-        # Flip to the other player
-        flip_player()
-
-    # The Game has ended
-    if winner == 'X' or winner == 'O':
-        print(winner + ' won.')
-    elif winner == None:
-        print('The Game Is A Tie.')
-
-
-    handle_turn()
 
 def handle_turn(player):
     position = input('Choose a position from 1-9: ')
@@ -118,67 +110,67 @@ def check_win():
     return
 
 def check_rows():
-    # # set global variable
-    # global game_continues
-    # # check if rows have same value and not '-'
-    # row_1 = board[7] ==  board[8] == board[9]  != '_'
-    # row_2 = board[4] ==  board[5] == board[6]  != '_'
-    # row_3 = board[1] ==  board[2] == board[3]  != '_'
+    # set global variable
+    global game_continues
+    # check if rows have same value and not '-'
+    row_1 = board[7] == board[8] == board[9] != '_'
+    row_2 = board[4] == board[5] == board[6] != '_'
+    row_3 = board[1] == board[2] == board[3] != '_'
     
-    # # if column has a match, then flag there is a winner
-    # if row_1 or row_2 or row_3:
-    #     game_continues = False
+    # if column has a match, then flag there is a winner
+    if row_1 or row_2 or row_3:
+        game_continues = False
     
-    # # return the winner (X or O)
-    # if row_1:
-    #     return board[7]
-    # elif row_2:
-    #     return board[4]
-    # elif row_3:
-    #     return board[1]
+    # return the winner (X or O)
+    if row_1:
+        return board[7]
+    elif row_2:
+        return board[4]
+    elif row_3:
+        return board[1]
 
     return
 
 
 
 def check_columns():
-    # # set global variable
-    # global game_continues
-    # # check if columns have same value and not '-'
-    # column_1 = board[1] ==  board[4] == board[7]  != '_'
-    # column_2 = board[2] ==  board[5] == board[8]  != '_'
-    # column_3 = board[3] ==  board[6] == board[9]  != '_'
+    # set global variable
+    global game_continues
+    # check if columns have same value and not '-'
+    column_1 = board[1] == board[4] == board[7] != '_'
+    column_2 = board[2] == board[5] == board[8] != '_'
+    column_3 = board[3] == board[6] == board[9] != '_'
 
-    # # if column has a match, then flag there is a winner
-    # if column_1 or column_2 or column_3:
-    #     game_continues = False
+    # if column has a match, then flag there is a winner
+    if column_1 or column_2 or column_3:
+        game_continues = False
 
     # # return the winner (X or O)
-    # if column_1:
-    #     return board[1]
-    # elif column_2:
-    #     return board[2]
-    # elif column_3:
-    #     return board[3]
+    if column_1:
+        return board[1]
+    elif column_2:
+        return board[2]
+    elif column_3:
+        return board[3]
     return
 
 
 def check_diagonals():
-    # # set global variable
-    # global game_continues
-    # # check if diagonals have same value and not '-'
-    # diagonal_1 = board[1] ==  board[5] == board[9]  != '_'
-    # diagonal_2 = board[3] ==  board[5] == board[7]  != '_'
+    # set global variable
+    global game_continues
+    # check if diagonals have same value and not '-'
+    diagonal_1 = board[1] == board[5] == board[9] != '_'
+    diagonal_2 = board[3] == board[5] == board[7] != '_'
     
-    # # if diagonal has a match, then flag there is a winner
-    # if diagonal_1 or diagonal_2:
-    #     game_continues = False
+    # if diagonal has a match, then flag there is a winner
+    if diagonal_1 or diagonal_2:
+        game_continues = False
 
-    # # return the winner (X or O)
-    # if diagonal_1:
-    #     return board[5]
-    # elif diagonal_2:
-    #     return board[7]
+    # return the winner (X or O)
+    if diagonal_1:
+        return board[1]
+    elif diagonal_2:
+        return board[7]
     
     return
 
